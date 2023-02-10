0134171f76a392b4815c9062952628df098150c3
Stephen <stephenvdw7777@gmail.com>
2023-02-10 15:31:03 +0200

    Added home & about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # to make home page available from /home
+  #   get 'home', to: 'pages#home'
+  #   get 'pages/about'
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'pages#home'
 end
