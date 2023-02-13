9c62820ef94a18b9d088dd88c9ca507d67f608b3
Stephen <stephenvdw7777@gmail.com>
2023-02-13 20:47:42 +0200

    Tried to fix rich text area not working

--- vendor/bundle/ruby/3.0.0/gems/stimulus-rails-1.2.1/lib/install/app/javascript/controllers/application.js
+++ vendor/bundle/ruby/3.0.0/gems/stimulus-rails-1.2.1/lib/install/app/javascript/controllers/application.js
@@ -1,9 +1,11 @@
 import { Application } from "@hotwired/stimulus"
+import "@rails/actiontext"
+import "trix"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
